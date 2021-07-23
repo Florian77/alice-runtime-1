7d86eb8bff8872ca0be907d0a2192fe0bc79835a
Florian <info@ftws.de>
2021-07-23 13:14:22 +0200

    remove dev-test + examples

--- dev-test/app1/app1-env.js
+++
@@ -1,6 +0,0 @@
-// const pack = require('../package');
-
-process.env.ALICE_RUNTIME_DRIVER = "mongodb";
-process.env.ALICE_RUNTIME_MONGODB_URL = "mongodb://localhost";
-process.env.ALICE_RUNTIME_MONGODB_DB = `alice-dev-test-app1`;
-
--- dev-test/app1/app1-test-env.js
+++
@@ -1,2 +0,0 @@
-require('./app1-env');
-process.env.ALICE_RUNTIME_MONGODB_DB = process.env.ALICE_RUNTIME_MONGODB_DB+'-test';
