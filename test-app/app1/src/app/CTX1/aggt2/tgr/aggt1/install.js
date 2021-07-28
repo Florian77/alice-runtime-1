2e4787d3d5e9040aa9deb2aeaa844ec602852d89
Florian <info@ftws.de>
2021-07-28 16:19:47 +0200

    dev

--- test-app/app1/src/app/CTX1/aggt2/tgr/aggt1/install.js
+++ test-app/app1/src/app/CTX1/aggt2/tgr/aggt1/install.js
@@ -3,11 +3,20 @@ const {CTX1__AGGT2} = require("../../../../../app_aggregates");
 const {CTX1__AGGT1} = require("../../../../../app_aggregates");
 const path = require('path')
 
+const useIt = R.ap(
+    R.flip(R.drop),
+    R.pipe(
+        R.lastIndexOf(path.sep),
+        R.add(1)
+    )
+)
+
+
 const installTrigger = {
     type: "stream",
     context: CTX1__AGGT2.context,
     aggregate: CTX1__AGGT2.aggregate,
-    trigger: R.pipe(R.split(path.sep), R.last)(__dirname),
+    trigger: useIt(__dirname), // R.pipe(R.split(path.sep), R.last)(__dirname),
     streamContext: CTX1__AGGT1.context,
     streamAggregate: CTX1__AGGT1.aggregate,
     streamAggregateId: null,
@@ -15,5 +24,15 @@ const installTrigger = {
     paused: true,
 };
 
+/*
+// in the body of getDir() ...
+-> R.lastIndexOf
+var sepIndex = filePath.lastIndexOf('/');
+if(sepIndex == -1){
+    sepIndex = filePath.lastIndexOf('\\');
+}
+// include the trailing separator
+return filePath.substring(0, sepIndex+1);
+*/
 
 module.exports = installTrigger;
